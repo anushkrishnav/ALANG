84ed2def795cecf00683dd39bef7ae3a5d3f2d34
anushkrishnav <anush.venkatakrishna@gmail.com>
2020-07-04 23:25:01 +0530

    implemented AST

---
+++ SourceCode/ASTree.py
@@ -0,0 +1,24 @@
+from Token import Token
+from Lexer import Lexer
+INTEGER,PLUS,EOF,MINUS,POW,DIV,MUL,LPAR,RPAR = 'INTEGER', 'PLUS', 'EOF','MINUS','POW','DIV','MUL', 'LPAR' , 'RPAR'
+OPERATORS={'+':PLUS,'-':MINUS,'^' :POW,'/':DIV,'*':MUL}
+
+class AST(object):
+    pass
+
+class BinOP(AST):
+    def __init__(self,left,op,right):
+        self.left=left
+        self.token=self.op=op
+        self.right=right
+class Num(AST):
+    def __init__(self,Token):
+        self.Token=Token
+        self.value=Token.value
+class NodeVisitor(object):
+    def visit(self,node):
+        method_name = 'visit_' + type(node).__name__
+        visitor = getattr(self, method_name, self.generic_visit)
+        return visitor(node)
+    def generic_visit(self, node):
+        raise Exception('No visit_{} method'.format(type(node).__name__))
